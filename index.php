9feddf48ad6a370c475f8c161252d33c676addf1
Talita Fiacadori <tvfiacadori@gmail.com>
2022-11-21 12:14:26 +0100

    update DB

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 // TODO : autoload classes.
- include "./classes/Form.class.php"
+spl_autoload_register(function ($class) {
+  include $class . '.class.php';
+});
 ?>
 
 <h1>COMBAT ARENA Hello</h1>
 
 <?php
+
+// connection DB
+
+$database = new Database("localhost", "root", "combat_dices", "");
+
+$database->connect();
+// $database->prepReq("SELECT * FROM perssonage");
+$personnage = $database->fetchData();
+
+
+
+
 // Generate a form to add fighter to the bdd.
 
 $formulaire = new Form("./pages/arena.php", "GET");
